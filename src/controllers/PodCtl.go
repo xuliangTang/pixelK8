4d87728dcdc53f818a2220f3f33dbe40cb89caee
xuliangTang <1297441127@qq.com>
2022-12-27 18:06:56 +0800

    feat: paging pods list

--- src/controllers/PodCtl.go
+++ src/controllers/PodCtl.go
@@ -16,10 +16,28 @@ func NewPodCtl() *PodCtl {
 	return &PodCtl{}
 }
 
-func (this *PodCtl) pods(ctx *gin.Context) any {
+func (this *PodCtl) pods(ctx *gin.Context) athena.Collection {
+	page := athena.NewPageWithCtx(ctx)
 	ns := ctx.DefaultQuery("ns", properties.App.K8s.DefaultNs)
 	podList := this.PodService.ListByNs(ns)
-	return podList
+
+	// pod总数和就绪数
+	var count, countReady int
+	iPodList := make([]any, len(podList))
+	for i, pod := range podList {
+		iPodList[i] = pod
+		count++
+		if pod.IsReady {
+			countReady++
+		}
+	}
+
+	page.Extend = gin.H{"count": count, "count_ready": countReady}
+	// 分页
+	start, end := page.SlicePage(iPodList)
+	collection := athena.NewCollection(podList[start:end], page)
+
+	return *collection
 }
 
 func (this *PodCtl) Build(athena *athena.Athena) {
